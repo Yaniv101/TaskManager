58f8d8746945f5c097b13d9164c5af8453efda95
Yaniv Aviel <Yaniv101@gmail.com>
2020-10-25 11:54:32 +0200

    mmm

--- MatrixTaskManager/TaskManager/Matrix.TaskManager.Tester/DbScript/Tables/dbo.Table_1.sql
+++ MatrixTaskManager/TaskManager/Matrix.TaskManager.Tester/DbScript/Tables/dbo.Table_1.sql
@@ -3,6 +3,7 @@ CREATE TABLE [dbo].[Table_1]
 [f1] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [f2] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [f3] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-[f4] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+[f4] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[f5] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY]
 GO
---
+++ MatrixTaskManager/TaskManager/Matrix.TaskManager.Tester/DbScript/Tables/dbo.Table_2.sql
@@ -0,0 +1,6 @@
+CREATE TABLE [dbo].[Table_2]
+(
+[f11] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[f22] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+) ON [PRIMARY]
+GO
