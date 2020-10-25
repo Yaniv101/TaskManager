74587d3cc92796b00c9661f3dda5d9bccc42c1ac
Yaniv Aviel <Yaniv101@gmail.com>
2020-10-25 11:38:56 +0200

    a

---
+++ MatrixTaskManager/TaskManager/Matrix.TaskManager.Tester/DbScript/script1MySQL_RS.sql
@@ -0,0 +1,21 @@
+Solution:
+
+DOP - X
+c:\Devel\DOP\Main\Source\DOP\
+
+
+DOP -OLD interact with sql plugin
+c:\Devel\Tools\Main\Source\RM\RMSolution.sln
+
+
+    DBmaestro.com/TWD/MSSQL/DBmaestroTeamWorkMssql.v4.9.7.zip
+​
+    ועוד משהו חשוב, אני אשלח לך איזה קוד אתה צריך להוריד מהsource control
+​
+    $/TeamWork/SQL/Versions/V4.9.7
+​
+    $/TeamWork/Framework/Versions/V4.9.7
+​
+
+	$/TeamWork/Tools/Versions/V4.9.7
+
